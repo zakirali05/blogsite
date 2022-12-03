74981638b93179252b9fab03427274cf899b7595
zakirali05 <109236535+zakirali05@users.noreply.github.com>
2022-12-03 13:15:32 +0530

    all routes set up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import { useParams } from "react";
+// import { useHistory } from "react-router-dom";
+import Header from "./Header";
+import Nav from "./Nav";
+import Home from "./Home";
+import AddPost from "./AddPost";
+import PostPage from "./PostPage";
+import About from "./About";
+import Footer from "./Footer";
+import Missing from "./Missing";
+import React from "react";
+// import { ReactDOM } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useHistory,
+} from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <h1>hi</h1>
+      <Router>
+        <Header />
+        <Nav />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+
+          <Route path="/addpost" element={<AddPost />} />
+
+          <Route path="/about" element={<About />} />
+
+          <Route path="/postpage/:id" element={<PostPage />} />
+          <Route path="*" element={<Missing />} />
+        </Routes>
+
+        <Footer />
+      </Router>
     </div>
   );
 }
