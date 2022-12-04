97072eb60f45144146acc0674b726f15b3f10873
zakirali05 <109236535+zakirali05@users.noreply.github.com>
2022-12-04 11:28:59 +0530

    footer fixed on home page!!

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
   const date = new Date();
   const year = date.getFullYear();
   return (
-    <footer className="h-8 relative bottom-0 p-6 bg-blue-400 text-white font-bold flex items-center justify-center ">
+    <footer className="h-8 relative  bottom-0 p-6 bg-blue-400 text-white font-bold flex items-center justify-center ">
       <h2>copyright @ {year}</h2>
     </footer>
   );
--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,7 @@ import Feed from "./Feed";
 
 const Home = ({ items }) => {
   return (
-    <main align="center" className="flex-col mb-40  h-[100vh]">
+    <main align="center" className="flex-col mb-8  h-[100%]">
       {items.length ? (
         <Feed items={items} />
       ) : (
